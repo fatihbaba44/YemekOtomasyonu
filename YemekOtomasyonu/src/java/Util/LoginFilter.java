115f178713f8b5b237f676a3297113589c1060e5
Pc <Pc@192.168.1.195>
2020-06-08 17:40:52 +0300

    LoginFilter util sınıfı oluşturuldu ve son haline getirildi

---
+++ YemekOtomasyonu/src/java/Util/LoginFilter.java
@@ -0,0 +1,61 @@
+package util;
+
+import entity.User;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author fth
+ */
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String url = req.getRequestURI();
+
+        User user = (User) req.getSession().getAttribute("valid_user");
+
+        if (user == null) {
+
+            if (url.contains("Form") || url.contains("Logout")) {
+                res.sendRedirect("http://localhost:8080/YemekOtomasyonu/module/Login/Login.xhtml");
+            } else {
+                chain.doFilter(request, response);
+            }
+        } else {
+            if (url.contains("Login")/*||url.contains("normalKullanici")*/) {
+                res.sendRedirect("http://localhost:8080/YemekOtomasyonu/module/index.xhtml");
+
+            } else if (url.contains("Logout")) {
+                req.getSession().invalidate();
+                res.sendRedirect("http://localhost:8080/YemekOtomasyonu/module/Login/Login.xhtml");
+            } else {
+                chain.doFilter(request, response);
+            }
+
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
